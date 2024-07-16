eb277e685c885ee33901cda63bf8a61378159843
tog <96715682+utogtokh6@users.noreply.github.com>
2024-07-16 20:20:07 +0800

    Testeer ehnii commit-iig hiilee

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ var scores = [0,0,];
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random()*6)+1;
 
-{/* <div class="player-score" id="score-0">43</div> */}
-document.querySelector("#score-0").textContent = dice;
 
-{/* <div class="player-score" id="score-1">43</div> */}
-document.querySelector("#score-1").textContent = dice;
 
 //Програм эхлэхэд бэлтгэе
-document.querySelector(".dice").style.display="none";
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+
+var diceDom = document.querySelector(".dice");
+
+diceDom.style.display="none";
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var DiceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.style.display="block";
+    diceDom.src = 'dice-'+ DiceNumber +'.png';
+
+})
