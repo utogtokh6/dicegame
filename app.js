edd5ee4ef1ee9752252eb44378d9c59d7d08122c
tog <96715682+utogtokh6@users.noreply.github.com>
2024-07-16 21:32:23 +0800

    toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+
+var activePlayer = 0;
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-var scores = [0,0,];
+
+var scores = [0,0];
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+
 var roundScore = 0;
-//Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-
-
 
 //Програм эхлэхэд бэлтгэе
 
@@ -15,13 +15,31 @@ document.getElementById("score-1").textContent = 0;
 document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 
-
 var diceDom = document.querySelector(".dice");
-
 diceDom.style.display="none";
 document.querySelector(".btn-roll").addEventListener("click", function(){
-    var DiceNumber = Math.floor(Math.random()*6)+1;
-    diceDom.style.display="block";
-    diceDom.src = 'dice-'+ DiceNumber +'.png';
 
-})
+    //Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+    var diceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.style.display="block";
+    diceDom.src = 'dice-'+ diceNumber +'.png';
+
+//Шоо нэгээс ялгаатай тоотой буусан тохиолдолд идэвхитэй тоглогчийн оноог нэмж өгнө.
+    if (diceNumber !== 1 ) {
+        roundScore = roundScore + diceNumber;
+
+        document.getElementById("current-"+ activePlayer).textContent = roundScore;
+    } else {
+
+        document.getElementById("current-"+ activePlayer).textContent = 0;
+
+        if (activePlayer === 0) {activePlayer = 1;
+            document.querySelector('.player-0-panel').classList.toggle("active");
+            document.querySelector('.player-1-panel').classList.toggle("active");
+
+        } else {activePlayer = 0;
+
+         };
+         diceDom.style.display="none";
+    }
+});
